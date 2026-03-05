1494019fad60bef4dee3b10e0efaffc0b5141a36
Claudio888 <claudiogoncalves@handtalk.me>
2026-03-05 14:16:31 -0300

    feat: config.py and secret files

--- dundie/config.py
+++ dundie/config.py
@@ -2,7 +2,7 @@
 
 import os
 
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,4 +13,8 @@ settings = Dynaconf(
     environments=["development", "production", "testing"],
     env_switcher="dundie_env",
     load_dotenv=False,
+)
+
+settings.validators.register(
+    Validator("security.SECRET_KEY", must_exist=True, is_type_of=str)
 )
